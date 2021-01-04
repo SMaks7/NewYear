5fc15d685f2d9ac7102016b1fad7fc1372799409
Smaks <smaks.biz@gmail.com>
2021-01-04 11:49:38 +0500

    NewYear 05.01

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ var stop bool
 
 func HomeRouterHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "Уже 4 января")
+	fmt.Fprintf(w, "Уже 5 января")
 	stop = true
 }
 func main(){
